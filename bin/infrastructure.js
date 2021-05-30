feeb55d02229d40090b6bced08b1a58b148c888e
Alejandro Guerrero <alexgue404@gmail.com>
2021-05-30 19:32:02 +0200

    feat: Updating for v2 infrastructure

--- bin/infrastructure.js
+++ bin/infrastructure.js
@@ -1,17 +1,23 @@
 #!/usr/bin/env node
 
-const cdk = require('@aws-cdk/core');
+const cdk = require('aws-cdk-lib');
+const { EcrImageCode } = require('aws-cdk-lib/lib/aws-lambda');
 const { InfrastructureStack } = require('../lib/infrastructure-stack');
 
 const app = new cdk.App();
-new InfrastructureStack(app, 'H0ck-Infrastructure', {
+
+// let imagesStack = new ECRImagesStack(app, 'ECRImagesStack', {
+
+// });
+
+let infrastructureStack = new InfrastructureStack(app, 'InfrastructureStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
 
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
-   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: "eu-west-3" },
+  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
@@ -19,3 +25,6 @@ new InfrastructureStack(app, 'H0ck-Infrastructure', {
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
+
+//infrastructureStack.addDependency(imagesStack);
+
--- test/infrastructure.test.js
+++ test/infrastructure.test.js
@@ -1,5 +1,4 @@
-const { expect, matchTemplate, MatchStyle } = require('@aws-cdk/assert');
-const cdk = require('@aws-cdk/core');
+const cdk = require('aws-cdk-lib');
 const Infrastructure = require('../lib/infrastructure-stack');
 
 test('Empty Stack', () => {
@@ -7,7 +6,6 @@ test('Empty Stack', () => {
     // WHEN
     const stack = new Infrastructure.InfrastructureStack(app, 'MyTestStack');
     // THEN
-    expect(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+    const actual = app.synth().getStackArtifact(stack.artifactId).template;
+    expect(actual.Resources || {}).toEqual({});
 });
